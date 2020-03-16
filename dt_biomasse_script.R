6100e1ef402dc36084ade4b3a347ecf9ed291624
loubnaelmadouri <you@example.com>
2020-03-16 14:50:02 +0100

    pb changement types variables

--- dt_biomasse_script.R
+++ dt_biomasse_script.R
@@ -23,15 +23,40 @@ biomass_unnest <-unnest(bystation_biomass, coeff)
 
 #Suppression des colonnes inutiles
 biomass_coeff <- biomass_unnest[ , - c(2:3)]
-
+str(biomass_coeff)
+##OK
 #Suppression des parenthèses
-data<-biomass_coeff
+data <- mutate(biomass_coeff,
+                str_replace_all, pattern = "(Intercept)", replacement = "Intercept")
+library(stringi)
+data <- stri_replace_all_fixed(biomass_coeff$term, ")", "")
+
+gsub("\s*\([^\)]+\)","",as.character(biomass_coeff$term))
+
+library(stringr)
+str_replace(biomass_coeff$term, "\(.*\)", "")
+
+####
+str(df)
+#C'est là que les estimate deviennent des caractères...
 data[-1] <- lapply(data[-1], gsub, pattern = "(Intercept)", replacement = "Intercept", fixed = TRUE)
 data[-1] <- lapply(data[-1], gsub, pattern = "nb_year", replacement = "Pente", fixed = TRUE)
+## Réessayer avec str_replace_all. str_replace_all pour vecteurs ? 
+df <- mutate_if(data, 
+                is.numeric,
+                str_replace_all, pattern = "(Intercept)", replacement = "Intercept")
+str(df)
+# essai avec un data frame modifié à la base 
+#Caractères aussi 
 #Division en deux du data frame
 data_part1 <- dplyr::select(data, station, term, estimate) %>%
   pivot_wider(names_from = term, values_from = estimate)
+#Essai : spread
+data_part1_bis <- dplyr::select(data, station, term, estimate)
+data_part1_spread <- tidyr::spread(data_part1_bis, term, estimate)
+str(data_part1_bis)
 
+str(data_part1)
 #Supprimer les lignes intercept du data_part2
 data_part2 <- dplyr::select(data, station, term, std.error, statistic, p.value)
 data_part3 <- data_part2[seq(2, nrow(data_part2), 2),] %>% 
@@ -39,3 +64,5 @@ data_part3 <- data_part2[seq(2, nrow(data_part2), 2),] %>%
 
 #Fusionner les deux jeux de données
 dt_biomass <- left_join(data_part1, data_part3, by = "station")
+
+#ungroup
